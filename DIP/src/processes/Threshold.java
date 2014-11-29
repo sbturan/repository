c5e50895d43a4f0550eb550dc807143c230a572b
seckin turan <sb_turan@hotmail.com>
2014-11-30 00:22:07 +0200

    iterative threshold. hata var ama yöntemde. çözemedim

--- DIP/src/processes/Threshold.java
+++ DIP/src/processes/Threshold.java
@@ -2,6 +2,8 @@ package processes;
 
 import java.awt.Color;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Threshold extends Process {
 
@@ -41,6 +43,78 @@ public class Threshold extends Process {
             }
         }
     }
+    
+    public void iterativeThreshold(int thresholdLevel)
+	{  // threshold level, iterasyonun durma noktasý
+		
+		
+
+		
+		Color threshold=new Color(128,128,128);
+		
+		Color lastThreshold=iterate(threshold, thresholdLevel);
+		int imgWidht=img.getWidth();
+ 		int imgHeight=img.getHeight();
+ 		Color white=new Color(0,0,0);
+ 		Color black=new Color(255,255,255);
+ 		
+ 		for(int i=0;i<imgWidht;i++){
+ 			for(int j=0;j<imgHeight;j++){
+ 				if(img.getRGB(i, j)<lastThreshold.getRGB()){
+ 					outputImg.setRGB(i, j, white.getRGB());
+ 				}else{
+ 					outputImg.setRGB(i, j, black.getRGB());
+ 				}
+ 			}
+ 		}
+		
+	
+	}
+	private Color iterate(Color oldThreshold,int thresholdLevel){
+		
+     	ArrayList<Integer> g1=new ArrayList<Integer>();
+ 		ArrayList<Integer> g2=new ArrayList<Integer>();
+ 		
+ 		int imgWidht=img.getWidth();
+ 		int imgHeight=img.getHeight();
+ 		for(int i=0;i<imgWidht;i++){
+ 			for(int j=0;j<imgHeight;j++){
+ 				if(img.getRGB(i,j)<oldThreshold.getRGB()){
+ 					g1.add(img.getRGB(i,j));
+ 					
+ 				}else{
+ 					g2.add(img.getRGB(i,j));
+ 				}
+ 				
+ 			}}
+ 		
+ 		
+         int c1=getAvarage(g1);
+        int c2=getAvarage(g2);
+        Color newThreshold=new Color((c1+c2/2));
+		int itl=newThreshold.getRGB()-oldThreshold.getRGB();
+		if(itl<0){
+			itl=itl*(-1);
+		}
+		System.out.println("oldThreshold//New threshold//c1//c2//g1//g2");
+		System.out.println(oldThreshold.getRGB()+"//"+newThreshold.getRGB()+"//"+c1+"//"+c2+"//"+g1.size()+"//"+g2.size());
+		if(itl>thresholdLevel){
+			newThreshold=iterate(newThreshold,thresholdLevel);
+			
+		}
+		
+		
+	return newThreshold;	
+	}
+	private int getAvarage(List<Integer> list){
+		int t=0;
+		for(int i=0;i<list.size();i++){
+			t =t+list.get(i);
+			
+		}
+		
+		return t/list.size();
+	}
 
     @Override
     public void run() {
